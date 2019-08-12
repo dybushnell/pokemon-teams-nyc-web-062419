54caf196157372172dbc6c28be7284748c09919e
Dave Bushnell <dybushnell@gmail.com>
2019-08-12 15:17:01 -0400

    done

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,6 +1,11 @@
-require 'faker'
+require "faker"
 
 class PokemonsController < ApplicationController
+  def index
+    @pokemons = Pokemon.all
+    render json: @pokemons
+  end
+
   def create
     unless pokemon_params[:trainer_id].nil?
       default = {}
@@ -11,16 +16,16 @@ class PokemonsController < ApplicationController
         end
 
         if pokemon_params[:species].nil?
-          default[:species] = Faker::Pokemon.name
+          default[:species] = Faker::Games::Pokemon.name
         end
 
         @pokemon = Pokemon.create(pokemon_params.merge(default))
         render json: @pokemon, status: 201
       else
-        render json: { error: "Party is Full!"}, status: 403
+        render json: { error: "Party is Full!" }, status: 403
       end
     else
-      render json: { error: "Trainer not found"}, status: 404
+      render json: { error: "Trainer not found" }, status: 404
     end
   end
 
@@ -35,6 +40,7 @@ class PokemonsController < ApplicationController
   end
 
   private
+
   def pokemon_params
     params.require(:pokemon).permit(:nickname, :species, :trainer_id)
   end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,92 @@
-const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
+    const BASE_URL = "http://localhost:3000"
+    const TRAINERS_URL = `${BASE_URL}/trainers`
+    const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+    const cardContainer = document.querySelector("main")
+
+    document.addEventListener("DOMContentLoaded", () => {
+
+
+
+
+
+        fetch(TRAINERS_URL)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(trainer => {
+                    cardContainer.insertAdjacentHTML("beforeend",
+                        `<div class="card" data-id=${trainer.id}> 
+                    <p> ${trainer.name} </p> 
+                    <button class="add" data-trainer-id=${trainer.id}> Add Pokemon </button> 
+                    <ul id = "list-${trainer.id}">     
+                    </ul>
+                    </div>`
+                    ) // end insertAdjacentHTML for trainers
+                    trainer.pokemons.forEach(pokemon => {
+                        let trainers_pokemons_list = document.getElementById(`list-${trainer.id}`)
+                        trainers_pokemons_list.insertAdjacentHTML("beforeend",
+                            `<li> ${pokemon.nickname} (${pokemon.species}) <button class="release"
+                    data-pokemon-id="${pokemon.id}" > Release </button></li>`
+
+                        ) // end inserAdjacentHTML for pokemons
+                    }) // end trainer.pokemons.forEach
+                }) // end date.forEach
+            }) // end second then
+        ; // end fetch
+
+        // DELETE pokemon
+        cardContainer.addEventListener("click", function (e) {
+            let deleteBtn = e.target.className
+            if (deleteBtn === "release") {
+                let pokemonId = e.target.dataset.pokemonId
+                fetch(`http://localhost:3000/pokemons/${pokemonId}`, {
+                        method: "delete"
+                    })
+                    .then(response => response.json()
+                        .then(pokemon => {
+                            allReleaseBtns = document.querySelectorAll(".release")
+                            allReleaseBtns.forEach(button => {
+                                if (button.dataset.pokemonId === pokemonId) {
+                                    button.parentElement.remove()
+                                }
+                            })
+                        }))
+            }
+        })
+
+        // ADD pokemon
+        cardContainer.addEventListener("click", function (e) {
+            let addBtn = e.target.className
+            if (addBtn === "add") {
+                let trainerId = e.target.dataset.trainerId
+                console.log(trainerId)
+                fetch(`http://localhost:3000/pokemons`, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Accept": "application/json"
+                        },
+                        body: JSON.stringify({
+                            "trainer_id": trainerId
+                        })
+                    })
+                    .then(response => response.json())
+                    .then(pokemon => {
+                        const trainers_pokemons_list = document.getElementById(`list-${trainerId}`)
+                        trainers_pokemons_list.insertAdjacentHTML("beforeend",
+                            `<li> ${pokemon.nickname} (${pokemon.species}) <button class="release"
+                            data-pokemon-id="${pokemon.id}" > Release </button></li>`)
+                    })
+            }
+        })
+
+
+
+
+
+
+
+
+
+
+    }) //end DOMContentLoaded event listener
